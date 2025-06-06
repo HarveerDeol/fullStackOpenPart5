98db7b0a46d6f9b72c9ec683248486b5630d76c9
Harveer Deol <harveerdeol53@gmail.com>
2025-06-06 13:35:23 -0600

    added more tests

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose')
 const logger = require('./logger.js')
 
 const PORT = process.env.PORT || 3001
-const url = process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test' 
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
 const connectToDb = async () =>{
     try { 
-        console.log('connecting to ', url)
-        mongoose.connect(url)
+        console.log('connecting to ', MONGODB_URI)
+        mongoose.connect(MONGODB_URI)
         logger.info('connected to Mongodb')
     } catch (error){
         logger.info('couldnt connect to Mongodb',error.message)
